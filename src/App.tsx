e0235e287fcf07b1e609fffbe693a60b42d60ff7
nongChaTea <869399522@qq.com>
2023-11-14 11:57:03 +0800

    2

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import UpdateElectron from '@/components/update';
 import logoVite from './assets/logo-vite.svg';
 import logoElectron from './assets/logo-electron.svg';
 import './App.css';
+import { Icon } from '@iconify/react';
 
 console.log('[App.tsx]', `Hello world from Electron ${process.versions.electron}!`);
 
@@ -21,11 +22,30 @@ function App() {
         </a>
       </div>
       <h1>Electron + Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+      <div className="card self-center">
+        <button onClick={() => setCount((count) => count + 1)}>
+          <Icon icon="humbleicons:arrow-right" />
+          count is {count}
+        </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
+      </div>
+      <div className="py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+        <img
+          className="block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0"
+          src="/public/icon.png"
+          alt="Woman's Face"
+        />
+        <div className="text-center space-y-2 sm:text-left">
+          <div className="space-y-0.5">
+            <p className="text-lg text-black font-semibold">Erin Lindford</p>
+            <p className="text-slate-500 font-medium">Product Engineer</p>
+          </div>
+          <button className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
+            Message
+          </button>
+        </div>
       </div>
       <p className="read-the-docs">Click on the Electron + Vite logo to learn more</p>
       <div className="flex-center">
